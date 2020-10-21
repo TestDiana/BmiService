083209556a078e3417e5ee7a658c85588e696a97
Diana Vafina <Dianochka.25@mail.ru>
2020-10-21 21:52:58 +0300

    .java added

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+
+    public float calculate(float weight, float height) {
+        float bmi = weight / ((height / 100) * (height / 100));
+        return bmi;
+
+    }
+}
